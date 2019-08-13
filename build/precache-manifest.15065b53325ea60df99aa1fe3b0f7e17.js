555c3f1c6bf86b154a8b574c03f053355ae7b653
Mateusz Stanielewicz <mstanielewicz@lulu.com>
2019-08-14 01:13:58 +0200

    Add build folder to repo

---
+++ build/precache-manifest.15065b53325ea60df99aa1fe3b0f7e17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81a3bb702d9757d21c4213d9a5cdf3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bafae537aaba1069e805",
+    "url": "/static/js/2.f7676b5f.chunk.js"
+  },
+  {
+    "revision": "3837970c3fe3611e4208",
+    "url": "/static/js/main.748d09d7.chunk.js"
+  },
+  {
+    "revision": "994edcf649b768976e3c",
+    "url": "/static/js/runtime~main.13185403.js"
+  }
+]);
